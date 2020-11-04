43c7aace25750fff463279b787d234b9305bedd0
MastaKoch <alexkoch1996@gmail.com>
2020-11-04 16:01:20 -0500

    got password to work. now i have to make sure it gives corresponding characters

--- script.js
+++ script.js
@@ -23,13 +23,10 @@ var password="";
 
 // create for loop to choose password characters
 for(var i=0; i <= complexity; i++){
-  password = password + values.charAt(Math.floor(Math.random()* Math.floor(values.length-1)));
+  password =  password + values.charAt(Math.floor(Math.random()* Math.floor(values.length-1)));
 }
 
-// add password to textbox/display area
-document.getElementById("generate").value = password;
-return generatePassword;
-
+return password;
 }
 
 // function to set length based on slider position
